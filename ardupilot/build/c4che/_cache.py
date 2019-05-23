542147255c68e950057512057d43c4a87962e83f
lychuang <lychuang11@gmail.com>
2019-05-23 16:22:42 +1000

    added build

---
+++ ardupilot/build/c4che/_cache.py
@@ -0,0 +1,6 @@
+AUTOCONFIG = True
+BINDIR = '/usr/bin'
+BOARD = 'stm32l475'
+DEBUG = False
+LIBDIR = '/usr/lib'
+PREFIX = '/usr'
---
+++ ardupilot/build/stm32l475/ap_version.h
@@ -0,0 +1,13 @@
+// auto-generated header, do not edit
+
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#define GIT_VERSION "e61b0b72"
+#define BUILD_DATE_YEAR 2019
+#define BUILD_DATE_MONTH 5
+#define BUILD_DATE_DAY 23
+#define CHIBIOS_GIT_VERSION "d443078c"
